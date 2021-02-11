e204e2ca64b3de87b127d643a48184d91697347f
ddayin <ddayin@gmail.com>
2021-02-11 23:23:17 +0900

    Convert.ToInt32()

---
+++ SystemConvert/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SystemConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringTest = "1234567890";
+
+            int intConverted = 0;
+            if (Int32.TryParse(stringTest, out intConverted) == true)
+            {
+                Console.WriteLine(intConverted + 10);
+            }
+
+            intConverted = Convert.ToInt32(stringTest);
+            Console.WriteLine(intConverted);
+        }
+    }
+}
