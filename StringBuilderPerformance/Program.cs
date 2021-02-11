57ab73f25ba6e1c8b8fb1b39abd775267bbd2a77
ddayin <ddayin@gmail.com>
2021-02-11 16:52:43 +0900

    second

---
+++ StringBuilderPerformance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestString();
+            TestStringBuilder();
+        }
+
+        private static void TestString()
+        {
+            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+
+            string test = String.Empty;
+            for (int i = 0; i < 10000; i++)
+            {
+                test += "HELLO WORLD! ";
+            }
+            long elapsed = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine(elapsed);
+        }
+
+        private static void TestStringBuilder()
+        {
+            var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 1000000; i++)
+            {
+                sb.Append("HELLO WORLD! ");
+            }
+            long elapsed = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine(elapsed);
+        }
+    }
+}
