9344af76c41a0211d0a426b77cbef7f632b9e5e0
ddayin <ddayin@gmail.com>
2021-02-11 15:48:17 +0900

    first

---
+++ CsharpForBeginners/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.
+            string test = "";
+
+            // 2.
+            test = String.Empty;
+
+            // 3.
+            if (String.IsNullOrEmpty(test) == true)
+            {
+                Console.WriteLine("string test is empty or null");
+            }
+        }
+    }
+}
