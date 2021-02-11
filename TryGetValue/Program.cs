4e445d1b12d6f568867c8e6d36d068f6d64f1c70
ddayin <ddayin@gmail.com>
2021-02-11 18:23:08 +0900

    third

---
+++ TryGetValue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryGetValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionaryTest = new Dictionary<string, int>();
+            dictionaryTest.Add("first", 0);
+            dictionaryTest.Add("second", 1);
+            dictionaryTest.Add("third", 2);
+
+            int value = 0;
+
+            // ContainsKey()
+            /*
+            if (dictionaryTest.ContainsKey("third") == true)
+            {
+                value = dictionaryTest["third"];
+                Console.WriteLine("For key = \"third\", value = {0}.", value);
+            }
+            else
+            {
+                Console.WriteLine("Key = \"third\" is not found.");
+            }
+            */
+
+            // TryGetValue()            
+            if (dictionaryTest.TryGetValue("third", out value))
+            {
+                Console.WriteLine("For key = \"third\", value = {0}.", value);
+            }
+            else
+            {
+                Console.WriteLine("Key = \"third\" is not found.");
+            }
+            
+        }
+    }
+}
